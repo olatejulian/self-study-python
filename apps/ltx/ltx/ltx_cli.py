ea7799c7cb7842cb21c014dcc32a0eebb88ac354
olatejulian <julian.lf.olate@gmail.com>
2024-10-04 00:27:40 -0300

    feat: :sparkles: add command to show configuration

--- apps/ltx/ltx/ltx_cli.py
+++ apps/ltx/ltx/ltx_cli.py
@@ -1,12 +1,14 @@
+import rich
 from typer import Typer
 
 from .ltx_container import GetLtxService
+from .ltxrc import JsonLtxrcParser
 
 ltx_cli = Typer(name="ltx")
 
 
 @ltx_cli.command()
-def ltx(
+def build(
     file_name: str,
 ):
     ltx_service = GetLtxService()
@@ -14,3 +16,32 @@ def ltx(
     ltx_service.post_init()
 
     ltx_service.latexmk(file_name)
+
+
+@ltx_cli.command()
+def config(absolute: bool = False):
+    ltxrc_parser = JsonLtxrcParser()
+
+    ltx_config = ltxrc_parser.get_configuration()
+
+    rich.print(
+        "Latexmk Arguments:",
+        *["  " + argument for argument in ltx_config["arguments"]],
+        sep="\n",
+        end="\n\n",
+    )
+
+    if absolute:
+        directories_title = "Directories (Absolute Paths):"
+
+    else:
+        directories_title = "Directories (Root Relative Paths):"
+
+    rich.print(
+        directories_title,
+        *[
+            f"  {dir_type.capitalize()}: {dir_name}"
+            for dir_type, dir_name in ltx_config["directories"].items()
+        ],
+        sep="\n",
+    )
